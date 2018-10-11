0401525a4b2e51a8f2d11133dc82c3731d7dd18a
Tony Waters <tw_ld@npd.learndirect.com>
2018-10-11 02:44:55 +0100

    Update version

--- up.sh
+++ up.sh
@@ -4,6 +4,10 @@
 if [ ! -f jira/docker/atlassian-jira-software-7.8.2.tar.gz ]; then
     echo "======> Downloading JIRA"
     wget https://www.atlassian.com/software/jira/downloads/binary/atlassian-jira-software-7.8.2.tar.gz -P jira/docker/
+fi
+
+if [ ! -f atlassian-jira-software-7.8.2-standalone ]; then
+    echo "======> Extracting JIRA"
     tar -xvzf jira/docker/atlassian-jira-software-7.8.2.tar.gz
 fi
 
