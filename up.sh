ce06f09f7bf5e2f23fc3bd006cd0d3c40dbaf33f
Tony Waters <tw_ld@npd.learndirect.com>
2018-10-11 02:42:30 +0100

    Update version

--- up.sh
+++ up.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# download JIRA if it does not exist
 if [ ! -f jira/docker/atlassian-jira-software-7.8.2.tar.gz ]; then
     echo "======> Downloading JIRA"
     wget https://www.atlassian.com/software/jira/downloads/binary/atlassian-jira-software-7.8.2.tar.gz -P jira/docker/
@@ -7,15 +8,19 @@ if [ ! -f jira/docker/atlassian-jira-software-7.8.2.tar.gz ]; then
 fi
 
 echo "======> Creating JIRA"
+docker-compose build
 docker-compose up -d
 
 #sleep 60
 
-#echo "======> Copying backup file to import location /var/atlassian/jira/import/"
-#docker cp backups/backup_jira.zip jira:/var/atlassian/jira/import/
+# copy backup if it exists
+if [ -f backups/backup_jira.zip ]; then
+    echo "======> Copying backup file to import location /var/atlassian/jira/import/"
+    docker cp backups/backup_jira.zip jira:/var/atlassian/jira/import/
+fi
 
 docker logs -f jira
-# docker run --publish 8080:8080 cptactionhank/atlassian-jira:6.4.11
+
 
 
 
